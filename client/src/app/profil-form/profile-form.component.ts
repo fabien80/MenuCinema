11b19da052621e1d7015248bbfbff21a876dda04
technet36 <ellipse.paro@gmail.Com>
2019-05-06 14:40:10 +0200

    profile form

--- client/src/app/profil-form/profile-form.component.ts
+++ client/src/app/profil-form/profile-form.component.ts
@@ -1,4 +1,6 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, Input, OnInit} from "@angular/core";
+import {ClientInterface} from "../interface/ClientInterface";
+import {NgForm} from "@angular/forms";
 
 
 @Component({
@@ -8,7 +10,30 @@ import {Component, OnInit} from "@angular/core";
 })
 
 export class profileFormComponent implements OnInit{
+
+    @Input() myClient:ClientInterface;
+
+
     ngOnInit(): void {
+        this.myClient = {
+            client_id: 123,
+            nom: "test",
+            prenom: "toto",
+            mail: "toto@test",
+            photo: "photo/path.png",
+            tel: "01 02 03 04 05",
+            fidelite: 12,
+            numero_rue: 124,
+            rue: "place de la rep",
+            ville: "SMH",
+            code_postal: 26200,
+            token: "",
+        };
+    }
+
+    onSubmit(){
+        console.log("This has to be sent to the back :");
+        console.log(this.myClient);
     }
 
 }
--- client/src/app/profil-form/profile-form.module.ts
+++ client/src/app/profil-form/profile-form.module.ts
@@ -1,13 +1,15 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {profileFormComponent} from "./profile-form.component";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
     declarations: [
         profileFormComponent
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        FormsModule
     ], exports: [
         profileFormComponent
     ]
