459147a5da97b38e1f003c8ae8b94749cdb35b59
Dinamots <jolan.daumas@gmail.com>
2019-05-07 11:02:27 +0200

    adding CommandeQueryInterface

---
+++ client/src/app/interface/CommandeInterface.ts
@@ -0,0 +1,11 @@
+interface CommandeQueryInterface {
+    foodsId: string;
+    moviesId: string;
+    menusId: string;
+    token: string;
+    rue: string;
+    numero_rue: number;
+    code_postal: string;
+    ville: string;
+    total: number;
+}
